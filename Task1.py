82fdb4d1394bf9995b1f16f37550d7076aff1fca
Atabek <atabekarapov14@gmail.com>
2020-11-02 12:25:30 +0600

    Week2_Task1

---
+++ Task1.py
@@ -0,0 +1,93 @@
+# Homework --> Atabek
+# 1)
+# name = "Hello World!"
+# print(len(name))
+
+# 2)
+# name = "Hello world!"
+# print(name.lower(), name.upper())
+
+# 3)
+# name = "Hellow world!"
+# print(name.replace('H', '$') .replace('!', '$'))
+
+# 4)  ????
+# a = input("Write your words: ")
+# b = (a.split( maxsplit = 4))
+# print(b)
+
+# 5)
+# a = input("Write your name: ")
+# b = "Hello, "
+# c = "!"
+# print(b + a + c)
+
+# 6) 
+# a = "Hello word I am backend developer!"
+# res = len(a.split())
+# print(res)
+
+# 7)
+# 7.1
+# a = "Hello world"
+# b = (a[2])
+# print(b)
+
+# 7.2
+# a = "Hello world"
+# b = (a[-1])
+# print(b)
+
+# 7.3
+# a = "Hello world"
+# print(a[0:5])
+
+# # 7.4
+# a = "Hello world"
+# print(a[0:10])
+
+# 7.5
+# a = "Hello world"
+# print(a[:12:2])
+
+# 7.6
+# a = "Hello world!"
+# print (a[1:12:2])
+
+# 7.7
+# a = "Hello world!"
+# print(a[::-1])
+
+# 7.8  ????
+# a = "Hello world!"
+# print(a[-1:2:-4])
+
+# 7.9
+# a = "Hello world!"
+# print(len(a))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
